d1abf93b661ad364907f61f4a9caf3a41957093b
Christos Panagiotakopoulos <chrispanag@gmail.com>
2019-06-07 15:25:21 +0300

    continue

--- lib/nmap.ts
+++ lib/nmap.ts
@@ -1,7 +1,21 @@
 import run from './run';
 
+export enum ScanType {
+    LIST_SCAN = '-sL',
+    NO_PORT_SCAN = '-sn',
+}
+
+export interface NmapOptions {
+    target: string;
+    scanType: ScanType
+}
+
 export default class Nmap {
     private args: string[] = [];
+
+    constructor(options: NmapOptions) {
+
+    }
 
     run() {
         const args = this.args.join(" ");
